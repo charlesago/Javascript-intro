ef450d7380eb3f43560018073316fc2a78ba2205
charlesagostinelli <chacha.agostinelli@gmail.com>
2022-10-10 13:00:29 +0200

    quelque changement

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
-console.log("coucou")
 
-console.log("lalala")
+
+let word = "Bonjour"
+
+let phrase = word.replace('o','');
+
+
+let neword = word.slice(3, 2)
+
+let mont = ['Jan', 'March', 'April', 'June']
+
+    mont.splice(1, 2, 'may');
+
+console.log(mont)
+console.log(neword)
+
+console.log(phrase)
+
+const monpara = document.querySelector( ".rouge")
+
+
+
+        setInterval(changeclass, 2000)
+
+function changeclass (){
+    monpara.classList.toggle("bleu")
+}
+fonction
+
+let mots = ["bonjour", "hola", "hello", "bonjornos"]
+
+
+
+
